6cd6288d1e083000bc7a721f84a6a89e15edc12d
kaimarin <kaimarin44@gmail.com>
2021-06-06 12:12:07 +0000

    add structure of about me page and projects

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,39 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+ function getShows() {
+  clearQuotes();
+  const imgs = ["images/IMG-9491.jpg", "images/IMG-9492.jpg", "images/IMG-9493.jpg",
+                "images/IMG-9494.jpg"];
+  const office = "A show I can watch over and over and it's hilarious every time";
+  const bb = "Breaking Bad: Crazy storytelling, will have you on the edge of your seat the whole way through";
+  const op = "An adventure story that is hilarious and inspiring";
+  const btc = "Prequel to Breaking Bad, really well written and acted.  Can't wait for season 6.";
+  const pars = [office, bb, op, btc];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random image which features quotes from my favorite TV shows and movies.
+  const index = Math.floor(Math.random() * imgs.length);
+  const src = imgs[index];
+  const p = pars[index];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const showsContainer = document.getElementById('shows-container');
+  let image = document.createElement("img");
+  image.src = src;
+  let par = document.createElement("p");
+  par.textContent = p;
+
+  showsContainer.appendChild(image);
+  showsContainer.appendChild(par);
+}
+
+/**
+ * Clears the current quote
+ */
+function clearQuotes() {
+  const showsContainer = document.getElementById('shows-container');
+  if (showsContainer.firstChild) {
+    showsContainer.removeChild(showsContainer.firstChild);
+    showsContainer.removeChild(showsContainer.firstChild);
+  }
 }
